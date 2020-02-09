007aed5efe5d13cd48cbc8fce08a757324517969
clash_m45 <clash.m45@gmail.com>
2020-02-09 17:05:11 +0900

    もっとも単純なGraphQLクエリが出来た

---
+++ README.md
@@ -0,0 +1,11 @@
+# photo-share-api
+
+初めてのGraphQL
+
+Node.jsでGraphQL APIを作る。
+
+## 開発の仕方
+
+* テスト用GraphQLサーバを起動する
+    * `npm start`
+    * `GraphQL Service running on http://localhost:4000/`
---
+++ index.js
@@ -0,0 +1,26 @@
+// apollo-serverモジュールを読み込む
+const {ApolloServer} = require(`apollo-server`);
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42
+  }
+};
+
+// サーバーのインスタンスを作成
+// その際、typeDefs(スキーマ)とリゾルバを引数に取る
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Webサーバーを起動
+server
+  .listen()
+  .then(({url}) => console.log(`GraphQL Service running on ${url}`));
