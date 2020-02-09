8dd2c47121cbb8b9fe743a46acc4b68bf20d910b
clash_m45 <clash.m45@gmail.com>
2020-02-09 20:44:37 +0900

    カスタムスカラー型

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // apollo-serverモジュールを読み込む
 const { ApolloServer } = require(`apollo-server`);
+const { GraphQLScalarType } = require(`graphql`);
 
 let _id = 0;
 
@@ -14,20 +15,23 @@ let photos = [
     "name": "Dropping the Heart Chute",
     "description": "The heart chute is one of my favorite chutes",
     "category": "ACTION",
-    "githubUser": "gPlake"
+    "githubUser": "gPlake",
+    "created": "3-28-1977"
   },
   {
     "id": "2",
     "name": "Enjoying the sunshine",
     "category": "SELFIE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "1-2-1985"
   },
   {
     "id": "3",
     "name": "Gunbarrel 25",
     "desctiprion": "25 laps on gunbarrel today",
     "category": "LANDSCAPE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "2018-04-15T19:09:57.308Z"
   }
 ];
 
@@ -66,6 +70,8 @@ const typeDefs = `
     LANDSCAPE
     GRAPHIC
   }
+  
+  scalar DateTime
 
   type Photo {
     id: ID!
@@ -73,6 +79,7 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    created: DateTime!
     postedBy: User!
     taggedUsers: [User!]!
   }
@@ -85,7 +92,7 @@ const typeDefs = `
 
   type Query {
     totalPhotos: Int!
-    allPhotos: [Photo]!
+    allPhotos(after: DateTime): [Photo]!
   }
   
   type Mutation {
@@ -98,14 +105,21 @@ const resolvers = {
     // 写真を格納した配列の長さを返す
     totalPhotos: () => photos.length,
     // すべての写真を返す
-    allPhotos: () => photos
+    allPhotos: (parent, args) => {
+      if (args.after) {
+        return photos.filter(p => p.created >= args.after);
+      } else {
+        return photos;
+      }
+    }
   },
 
   Mutation: {
     postPhoto(parent, args) {
       const newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        created: new Date()
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -118,7 +132,7 @@ const resolvers = {
       return users.find(u => u.githubLogin === parent.githubUser)
     },
     taggedUsers: parent => tags
-      .filter(tag => tag.photoID == parent.id)
+      .filter(tag => tag.photoID === parent.id)
       .map(tag => tag.userID)
       .map(userID => users.find(u => u.githubLogin === userID))
   },
@@ -131,7 +145,15 @@ const resolvers = {
       .filter(tag => tag.userID === parent.id)
       .map(tag => tag.photoID)
       .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+
+  DateTime: new GraphQLScalarType({
+    name: `DateTime`,
+    description: `A valid date time value`,
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 };
 
 // サーバーのインスタンスを作成
