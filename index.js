aec5c8cddf559ef49242a4aaf4dfafc7484557f7
clash_m45 <clash.m45@gmail.com>
2020-02-09 20:22:33 +0900

    一対多、多対多の接続

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 // apollo-serverモジュールを読み込む
-const {ApolloServer} = require(`apollo-server`);
+const { ApolloServer } = require(`apollo-server`);
+
+let _id = 0;
+
+let users = [
+  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+  { "githubLogin": "gPlake", "name": "Glen Plake" },
+  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+];
+let photos = [
+  {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes",
+    "category": "ACTION",
+    "githubUser": "gPlake"
+  },
+  {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+  },
+  {
+    "id": "3",
+    "name": "Gunbarrel 25",
+    "desctiprion": "25 laps on gunbarrel today",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+  }
+];
+
+let tags = [
+  {
+    "photoID": "1",
+    "userID": "gPlake"
+  },
+  {
+    "photoID": "2",
+    "userID": "sSchmidt"
+  },
+  {
+    "photoID": "2",
+    "userID": "mHattrup"
+  },
+  {
+    "photoID": "2",
+    "userID": "gPlake"
+  }
+]
 
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo]!
+  }
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -16,6 +73,8 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
   }
   
   input PostPhotoInput {
@@ -33,10 +92,6 @@ const typeDefs = `
     postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
-
-let _id = 0;
-// 写真を格納するための配列を定義する
-let photos = [];
 
 const resolvers = {
   Query: {
@@ -58,7 +113,24 @@ const resolvers = {
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${ parent.id }.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID == parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
+  },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 };
 
@@ -72,4 +144,4 @@ const server = new ApolloServer({
 // Webサーバーを起動
 server
   .listen()
-  .then(({url}) => console.log(`GraphQL Service running on ${url}`));
+  .then(({ url }) => console.log(`GraphQL Service running on ${ url }`));
